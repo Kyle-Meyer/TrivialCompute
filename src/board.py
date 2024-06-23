32748fe875abb6094cbe49c70318ad2de48222a5
kmeyer <kbm615@gmail.com>
2024-06-23 15:24:12 -0400

    player update
    couple of things changed:
    - player is now a circle
    - player has a clamp box that will restrict their movement to the radius of max number of squares as dictated by their dice roll
    - player mouse functionality moved to player class
    - player will simulate all possible moves recursively to ensure that the player can only move to dice appropriate slots
    - all tiles have collision detection now
    - added HQ tile type to the board
    - added "roll again" tile type to the board
    - removed vestigial code from main
    - set the frame rate to 60fps

--- src/board.py
+++ src/board.py
@@ -12,39 +12,43 @@ class cBoard(object):
     #P = head quarter tile
     #. = blank space   
 
-    template=[["X","X","X","X","X","X","X","X","X"],
+    template=[["R","X","X","X","H","X","X","X","R"],
              ["X",".",".",".","X",".",".",".","X"],
              ["X",".","P",".","X",".","P",".","X"],
              ["X",".",".",".","X",".",".",".","X"],
-             ["X","X","X","X","X","X","X","X","X"],
+             ["H","X","X","X","X","X","X","X","H"],
              ["X",".",".",".","X",".",".",".","X"],
              ["X",".","P",".","X",".","P",".","X"],
              ["X",".",".",".","X",".",".",".","X"],
-             ["X","X","X","X","X","X","X","X","X"],]
+             ["R","X","X","X","H","X","X","X","R"],]
     outerBoard = pygame.Rect(0, 0, 720 - 100, 720 - 100)
     outerBoard.center = (1280/2 , 720/2)
+    tileSize = 0
     #this will randomly generate tiles in the future
     def create_board(self):
         for i in range(9):
             for j in range(9):
                 match self.template[i][j]:
                     case "X":
+                        print("tile size", self.board[i][j].box.size)
                         self.board[i][j] = tile(triviaType.RED)
                         roll = True
                         newColor = random.choice(list(triviaType))
                         print("setting new tile at ", i, " , ", j, " to ", newColor, " from ", self.board[i][j].mTrivia)
                         self.board[i][j] = tile(newColor)
                     case "P":
-                        self.board[i][j] = tile(triviaType.RED, tileDistinction.HQ)
+                        self.board[i][j] = tile(triviaType.RED, tileDistinction.SPECIAL)
                     case ".":
                         self.board[i][j] = tile(triviaType.RED, tileDistinction.NULL)
+                    case "H":
+                        self.board[i][j] = tile(triviaType.RED, tileDistinction.HQ)
     #def setCore(self, limit : int):
     #    self.board[4][4].box =
     def correctBoard(self):
         for i in range(9):
             for j in range(9):
                 if self.board[i][j].mDistinct == tileDistinction.NORMAL:
-                    possibleColors = [triviaType.RED, triviaType.BLUE, triviaType.GREEN, triviaType.YELLOW]
+                    possibleColors = [triviaType.BLUE, triviaType.GREEN, triviaType.YELLOW, triviaType.RED, triviaType.RED]
                     if i - 1 > 0 and self.board[i-1][j].mTrivia in possibleColors:
                         possibleColors.remove(self.board[i-1][j].mTrivia)
                     if i + 1 < 9 and self.board[i+1][j].mTrivia in possibleColors:
--- src/tile.py
+++ src/tile.py
@@ -9,11 +9,14 @@ class triviaType(Enum):
     BLUE = 1
     YELLOW = 2
     GREEN = 3
+    WHITE = 4
 
 class tileDistinction(Enum):
     NORMAL = 0
     HQ = 1
-    NULL = 2
+    ROLL = 2
+    SPECIAL = 3
+    NULL = 4
 class tile(object):
     #standard convention in python to mark private variables with "__"
     
@@ -21,29 +24,42 @@ class tile(object):
     mColor = (base0)
     mDistinct = tileDistinction.NORMAL
     mTrivia = triviaType.RED
+
+    def is_inside_bounding_box(self, point_or_rect):
+        """ Check if a point or another rectangle is inside the bounding box. """
+        if isinstance(point_or_rect, pygame.Rect):
+            return self.box.colliderect(point_or_rect)
+        elif isinstance(point_or_rect, tuple):
+            return self.box.collidepoint(point_or_rect)
+        return False
+    
     def __init__(self, inColor=(base0), dist = tileDistinction.NORMAL, inSize = 10):
         self.size = inSize
         self.box = pygame.Rect(300, 200, self.size, self.size)
         self.mDistinct = dist
         self.Randomized=False
-        match inColor:
-            case triviaType.RED:
-                self.mTrivia = triviaType.RED
-                self.mColor = red
-            case triviaType.BLUE:
-                self.mTrivia = triviaType.BLUE
-                self.mColor = blue
-            case triviaType.YELLOW:
-                self.mTrivia = triviaType.YELLOW
-                self.mColor = yellow
-            case triviaType.GREEN:
-                self.mTrivia = triviaType.GREEN
-                self.mColor = green
         match dist:
             case tileDistinction.NULL:
                 self.mColor = null
             case tileDistinction.HQ:
                 self.mColor = base3
+            case tileDistinction.SPECIAL:
+                self.mColor = base00
+            case tileDistinction.NORMAL:
+                match inColor:
+                    case triviaType.RED:
+                        self.mTrivia = triviaType.RED
+                        self.mColor = red
+                    case triviaType.BLUE:
+                        self.mTrivia = triviaType.BLUE
+                        self.mColor = blue
+                    case triviaType.YELLOW:
+                        self.mTrivia = triviaType.YELLOW
+                        self.mColor = yellow
+                    case triviaType.GREEN:
+                        self.mTrivia = triviaType.GREEN
+                        self.mColor = green
+        
     def instantiateTile(self, inColor, inSize = 10):
         self.size = inSize
         self.box = pygame.Rect(300, 200, self.size, self.size)
